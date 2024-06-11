f7d81ff44ed970749b0ae16a1de8691a9bb17013
Dina Randriamifidy <randriamifidydina@gmail.com>
2024-06-11 17:24:33 +0300

    [int] :+1:  project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TotalContributions from "./Pages/TotalContributions";
+import PublicContributions from "./Pages/PublicContributions";
+import Followers from "./Pages/Followers";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicContributions />} />
+        <Route path="/total" element={<TotalContributions />} />
+        <Route path="/followers" element={<Followers />} />
+        <Route path="*" element={<TotalContributions />} />
+        <Route path="/simplex" element={<TotalContributions />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
